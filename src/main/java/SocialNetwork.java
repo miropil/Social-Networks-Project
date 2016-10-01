dd585be2c14fbb3821335257644ef0366e22f3fb
ilya <ilya@adallom.com>
2016-10-01 17:26:07 +0300

    project structure defined. sab=nity hello world passed

---
+++ src/main/java/SocialNetwork.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by ilyami on 10/1/2016.
+ *
+ */
+public class SocialNetwork implements Graph{
+    @Override
+    public void addVertex(int num) {
+
+    }
+
+    @Override
+    public void addEdge(int from, int to) {
+
+    }
+
+    @Override
+    public HashMap<Integer, HashSet<Integer>> exportGraph() {
+        return null;
+    }
+}
