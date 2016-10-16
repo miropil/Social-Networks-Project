bea27849d22265509877cb5b76a01619c302f66a
ilya <ilya@adallom.com>
2016-10-17 01:34:16 +0300

    added complexity worth case

--- src/main/java/CascadingNetwork.java
+++ src/main/java/CascadingNetwork.java
@@ -10,7 +10,7 @@ import java.util.*;
  */
 public class CascadingNetwork extends SocialNetwork{
     // nodes chosen to initiate an algorithm (will be removed from igniting nodes set as soon as used once)
-    private HashSet<Integer> startingNodes;
+    private HashSet<Integer> algorithmIntializingNodes;
 
     /**
      * Simulates cascade with intial conditions of ignitingNodes and threshold q
@@ -61,7 +61,7 @@ public class CascadingNetwork extends SocialNetwork{
      */
     public Set<Integer> findMinimalIgnitingSet(Set<Integer> ignitingSet, double q){
         // Check if current ignitingSet is enough
-        cascadeStartingFrom(ignitingSet, q);
+        cascadeStartingFrom(ignitingSet, q);                                                                            // O(n^2)
         if (switchedNodes.size() == nodes.keySet().size()){
             return ignitingSet;
         }
@@ -76,18 +76,18 @@ public class CascadingNetwork extends SocialNetwork{
         System.out.println("Border Nodes: " + borderNodes.getSet());
 
         // remove randomly selected nodes for algorithm initialization
-        for (int n: startingNodes){
+        for (int n: algorithmIntializingNodes){                                                                         //O(1)
             ignitingSet.remove(n);
         }
 
         // Since nodes were removed we won't remove them again if they appear as a result of running the algorithm
-        startingNodes = new HashSet<Integer>();
+        algorithmIntializingNodes = new HashSet<Integer>();
 
         // get all not switched nodes adjecent to the border
         for (Integer node: borderNodes.getSet()) {
             for (Node friend: nodes.get(node).getFriends()){
                 if (!switchedNodes.contains(friend.getId())){
-                    notSwitchedBorder.addNode(friend.getId(), true);
+                    notSwitchedBorder.addNode(friend.getId(), true);                                                    //O(n)
                 }
             }
         }
@@ -95,7 +95,7 @@ public class CascadingNetwork extends SocialNetwork{
         Set <Integer> probingSet = new HashSet<Integer>();
 
         // Find semi optimal nodes set to propagate the cascade further and add to igniting set
-        while (!cascadeStartingFrom(probingSet, q)&&!notSwitchedBorder.isEmpty()){
+        while (!cascadeStartingFrom(probingSet, q)&&!notSwitchedBorder.isEmpty()){                                      //O(n^3)
             int next =notSwitchedBorder.poll();
             probingSet.add(next);
             ignitingSet.add(next);
@@ -112,7 +112,7 @@ public class CascadingNetwork extends SocialNetwork{
     private void setStartingNodes(Set<Integer> ignitingNodes) {
         borderNodes = new Border();
         for(Integer node: ignitingNodes){
-            // use super explicitly to avoid adding node to startingNodes because this call to addStartingNode is part of main algorithm
+            // use super explicitly to avoid adding node to algorithmIntializingNodes because this call to addStartingNode is part of main algorithm
             super.addStartingNode(node);
         }
     }
@@ -122,10 +122,10 @@ public class CascadingNetwork extends SocialNetwork{
         super.addStartingNode(node);
 
         // add randomly selected initializing nodes to starting set to remove at the first iteration of the algorithm
-        if (startingNodes == null){
-            startingNodes = new HashSet<Integer>();
+        if (algorithmIntializingNodes == null){
+            algorithmIntializingNodes = new HashSet<Integer>();
         }
-        startingNodes.add(node);
+        algorithmIntializingNodes.add(node);
 
     }
 
